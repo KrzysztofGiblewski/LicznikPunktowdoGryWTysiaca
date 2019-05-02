58c0bbd0df78f1485b27c8d6a84e2ab6eecc52b3
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-05-02 17:45:05 +0200

    pierwszy

---
+++ app/src/main/java/com_krzysztofgiblewski/github/liczpunktydlagrywtysiaca/LiczPunktyGryWTysiaca.java
@@ -0,0 +1,13 @@
+package com_krzysztofgiblewski.github.liczpunktydlagrywtysiaca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LiczPunktyGryWTysiaca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_licz_punkty_gry_wtysiaca);
+    }
+}
