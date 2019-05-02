2503732ae4d11b6baaa9a7a7ad8960a0a5780eaa
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-05-02 21:43:00 +0200

    wyswietla sie a po wcisnieciu przycisku zmienia sie textView

--- app/src/main/java/com_krzysztofgiblewski/github/liczpunktydlagrywtysiaca/LiczPunktyGryWTysiaca.java
+++ app/src/main/java/com_krzysztofgiblewski/github/liczpunktydlagrywtysiaca/LiczPunktyGryWTysiaca.java
@@ -1,7 +1,7 @@
 package com_krzysztofgiblewski.github.liczpunktydlagrywtysiaca;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,11 +20,14 @@ public class LiczPunktyGryWTysiaca extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_licz_punkty_gry_wtysiaca);
         textViewPunktyPierwszego = (TextView) findViewById(R.id.textViewPunktyPierwszego);
+        textViewPunktyDrugiego = (TextView) findViewById(R.id.textViewPunktyDrugiego);
+        textViewPunktyTrzeciego = (TextView) findViewById(R.id.textViewPunktyTrzeciego);
+        przycisk = (Button) findViewById(R.id.button);
 
         przycisk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                textViewPunktyPierwszego.setText("100");
             }
         });
     }
