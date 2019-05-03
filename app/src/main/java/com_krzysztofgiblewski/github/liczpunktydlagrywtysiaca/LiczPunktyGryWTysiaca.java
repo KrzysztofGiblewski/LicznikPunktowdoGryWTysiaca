80f9bfc3dc247814abc36d3f28bd8bf185c378fd
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-05-03 16:55:20 +0200

    Toast dla karzdego zawodnika o niewpisanych punktach

--- app/src/main/java/com_krzysztofgiblewski/github/liczpunktydlagrywtysiaca/LiczPunktyGryWTysiaca.java
+++ app/src/main/java/com_krzysztofgiblewski/github/liczpunktydlagrywtysiaca/LiczPunktyGryWTysiaca.java
@@ -15,12 +15,18 @@ public class LiczPunktyGryWTysiaca extends AppCompatActivity {
     private TextView textViewPunktyPierwszego, textViewPunktyDrugiego, textViewPunktyTrzeciego;
     private EditText editTextNowePunktyPierwszego, editTextNowePunktyDrugiego, editTextNowePunktyTrzeciego;
 
+    private boolean pierwszy = true;
+    private boolean drogi = false;
+    private boolean trzeci = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_licz_punkty_gry_wtysiaca);
 
+        editTextName1 = (EditText) findViewById(R.id.editTextName1);
+        editTextName2 = (EditText) findViewById(R.id.editTextName2);
+        editTextName3 = (EditText) findViewById(R.id.editTextName3);
 
         textViewPunktyPierwszego = (TextView) findViewById(R.id.textViewPunktyPierwszego);
         textViewPunktyDrugiego = (TextView) findViewById(R.id.textViewPunktyDrugiego);
@@ -32,13 +38,18 @@ public class LiczPunktyGryWTysiaca extends AppCompatActivity {
 
         przycisk = (Button) findViewById(R.id.button);
 
+
         przycisk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
-                if (editTextNowePunktyPierwszego.length() == 0 || editTextNowePunktyDrugiego.length() == 0 || editTextNowePunktyTrzeciego.length() == 0)
-                    Toast.makeText(getApplicationContext(), "nie wpisałeś wszystkim punktów", Toast.LENGTH_LONG).show();
+                if (editTextNowePunktyPierwszego.length() == 0)
+                    Toast.makeText(getApplicationContext(), "nie wpisałeś " + editTextName1.getText().toString() + " punktów", Toast.LENGTH_LONG).show();
+                if (editTextNowePunktyDrugiego.length() == 0)
+                    Toast.makeText(getApplicationContext(), "nie wpisałeś " + editTextName2.getText().toString() + "  punktów", Toast.LENGTH_LONG).show();
+                if (editTextNowePunktyTrzeciego.length() == 0)
+                    Toast.makeText(getApplicationContext(), "nie wpisałeś " + editTextName3.getText().toString() + "  punktów", Toast.LENGTH_LONG).show();
 
                 if (editTextNowePunktyPierwszego.length() > 0 && editTextNowePunktyDrugiego.length() > 0 && editTextNowePunktyTrzeciego.length() > 0) {
 
